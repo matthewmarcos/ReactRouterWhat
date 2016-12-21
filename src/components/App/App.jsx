b454e7e1b93a745e85ce6a2c804a1c9c219fd883
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-21 19:48:45 +0800

    Axios and some boring shit. Now to get on with redux.... tomorrow

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+
+    handleClick() {
+        axios.get('/api')
+          .then(function (response) {
+              console.log(response);
+          })
+          .catch(function (error) {
+              console.log(error);
+          });
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Yeah!</h1>
+                <a className="waves-effect waves-light btn" onClick={this.handleClick.bind(this)}>button</a>
             </div>
         );
     }
