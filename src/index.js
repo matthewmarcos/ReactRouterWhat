944b8ea6b3a0db91f7b98b7777d838240adc51a3
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-21 19:23:57 +0800

    Change project structure, included Materialize.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import App from './components/App/App';
+
+import 'materialize-css/bin/materialize.js';
+import 'materialize-css/bin/materialize.css';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
