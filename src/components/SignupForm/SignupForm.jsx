feb75bfc4acfeb7df4798bf436399087f53d86d5
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-22 11:31:35 +0800

    Basic Sign up form and Change title

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -3,32 +3,77 @@ import React, { Component } from 'react';
 import './SignupForm.css';
 
 export default class SignupForm extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handleLogin(e) {
+        e.preventDefault();
+        console.log('Submitted form');
+        window.myForm = e.target;
+        console.log(e.target);
+        const { username, password } = this.state;
+
+        console.log('Username: ', username);
+        console.log('Password:', password);
+    }
+
+    handlePasswordChange(e) {
+        const password = e.target.value;
+        this.setState({ password });
+    }
+
+    handleUsernameChange(e) {
+        const username = e.target.value;
+        this.setState({ username });
+    }
+
     render() {
         return(
-            <div className="signup-form container">
-                <h1>Sign up form here lolol</h1>
-                <p>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis quas, nihil blanditiis, beatae, commodi error repudiandae maiores ullam atque facere ipsum obcaecati esse nobis hic, asperiores aspernatur necessitatibus distinctio. Accusantium.</div>
-                    <div>Vero dolorum provident repudiandae fuga. Odio soluta ad adipisci reiciendis error nostrum itaque illum voluptate eaque a. Iure nemo, sapiente itaque aut distinctio repellendus tenetur, eum ea accusantium est illum!</div>
-                    <div>Nemo culpa enim cum assumenda, iure porro eius sapiente maiores nulla, maxime suscipit necessitatibus, ipsam nesciunt quasi quisquam vel! Reprehenderit, voluptate molestias accusamus veritatis voluptatibus fuga et amet consectetur reiciendis.</div>
-                    <div>Distinctio, velit, neque! Excepturi ipsum aliquid dolores reprehenderit facilis hic laudantium, unde distinctio asperiores inventore fuga eius sunt illo laboriosam, deserunt exercitationem iure optio adipisci libero accusamus. Libero dolorem, debitis.</div>
-                    <div>Odit repellat, aliquam optio voluptatem odio veritatis ab aperiam? Reprehenderit cumque, ipsum minus, libero hic qui natus consectetur blanditiis dignissimos, repellendus suscipit dolor optio alias! Voluptatem repudiandae, cum sit qui!</div>
-                    <div>Quaerat cumque, ipsam nesciunt! Ut facere sed quam quis, perferendis quidem totam aspernatur iusto incidunt impedit aliquam illo excepturi. Laboriosam maxime aspernatur veritatis neque, itaque nam doloremque earum sit natus?</div>
-                    <div>Deleniti sed illo reiciendis, perferendis harum soluta aliquid libero dolor dignissimos magnam neque modi culpa laboriosam atque aliquam doloremque distinctio sit velit porro quis ea. Recusandae non voluptatum aliquam distinctio.</div>
-                    <div>Quae quam quisquam harum dolorem facere eum, delectus et, fuga eveniet veniam, corrupti asperiores obcaecati ab perspiciatis voluptate tempore. Consequatur, libero voluptas maxime velit eum inventore veniam. Quisquam, non tempora.</div>
-                    <div>Consequuntur numquam quaerat voluptatum saepe quia quos alias aperiam, iste corrupti autem perspiciatis, rerum, omnis molestias sint dolorum. Totam enim quidem minima hic voluptate. Quod deleniti aliquam cupiditate aspernatur quaerat?</div>
-                    <div>Unde nihil et, fuga repellat, quasi cum minus nam impedit ducimus. Veniam, porro enim odit temporibus cum atque assumenda voluptate perspiciatis officia est, ipsum sunt deleniti vero dicta itaque perferendis.</div>
-                    <div>Officiis et porro minima. Saepe quos esse repellendus iure a ullam dolorum alias pariatur eveniet ut, ab ipsum dolore accusantium nemo. Aliquid ducimus officiis, magni, dignissimos perferendis placeat. Blanditiis, inventore.</div>
-                    <div>Totam facilis eveniet dolores. Neque tempore, commodi similique quisquam, consectetur soluta? Laudantium repudiandae incidunt, aut atque eligendi maiores cumque, nam exercitationem vero aliquam rem reiciendis? Nobis aperiam iste numquam pariatur.</div>
-                    <div>Error libero ad commodi ipsam, earum molestiae pariatur aliquam, est quis! Optio nisi iste dolore numquam consequatur. Magnam ratione, nulla itaque perspiciatis perferendis natus neque sunt illo, provident obcaecati voluptatibus!</div>
-                    <div>Odit in inventore totam aperiam dolores, commodi vitae ratione pariatur distinctio quis necessitatibus officia qui porro natus esse nam voluptatum, nesciunt doloribus molestiae obcaecati consequuntur cum quod. Officiis, vel beatae?</div>
-                    <div>Modi dolorem nobis qui ratione quis hic quos expedita voluptatem maxime optio nulla molestiae necessitatibus ab, quia dicta et eaque labore est pariatur temporibus vitae. Quidem tenetur facilis sit sequi.</div>
-                    <div>Ex dicta eligendi obcaecati assumenda pariatur temporibus reprehenderit maiores libero. Saepe odio, maxime neque! Explicabo quia aliquam, quam, inventore error, vitae ex, cum itaque ipsam nostrum natus exercitationem repudiandae doloribus.</div>
-                    <div>Quis deleniti ut alias laudantium accusantium aut dolorem molestiae. Architecto consequatur obcaecati temporibus fugit illum molestiae adipisci nihil natus voluptatum delectus omnis laboriosam maiores accusantium recusandae libero earum dicta, quibusdam?</div>
-                    <div>Quod neque consectetur officiis veritatis, ratione magni esse labore hic ipsam aut laboriosam ut reiciendis iste quisquam, qui saepe. Quis eligendi mollitia asperiores pariatur cum sunt impedit reprehenderit tenetur, blanditiis!</div>
-                    <div>Corporis alias autem dolor porro error in tempora, veritatis ab est vel assumenda dolorum illo, quod voluptas distinctio voluptatibus hic velit minus! Necessitatibus possimus libero voluptas natus voluptatum exercitationem corporis!</div>
-                    <div>Perferendis, voluptatem, nulla id recusandae magni vel dolore repellendus laudantium quaerat ratione deleniti. Dolorem dignissimos ratione cum ea temporibus sunt laborum, illum sequi qui cumque quisquam quia enim. Rerum, mollitia!</div>
-                </p>
+            <div className="container">
+                <h2 className="header">Create Account</h2>
+                <div className="card horizontal">
+                    <div className="card-stacked">
+                        <div className="card-content">
+                        <form className="login-form" onSubmit={this.handleLogin.bind(this)}>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <i className="material-icons prefix">account_circle</i>
+                                <input id="username" onChange={this.handleUsernameChange.bind(this)} type="text" className="validate"/>
+                                <label htmlFor="username">Username</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <i className="material-icons prefix">lock</i>
+                                <input id="password" onChange={this.handlePasswordChange.bind(this)} type="password" className="validate"/>
+                                <label htmlFor="password">Password</label>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                <input id="password_retype" type="password" className="validate"/>
+                                <label htmlFor="password">Retype Password</label>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col offset-s10">
+                                    <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                                        <i className="material-icons right">send</i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
